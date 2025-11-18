509946a223f7e3bb01bc28d8fdd7e4287ca60b69
uzu-san <suguyarugogo@gmail.com>
2025-11-18 20:52:37 +0900

    Update gemini.js

--- pages/api/gemini.js
+++ pages/api/gemini.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     }
 
     const url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyCmNHNyyet1Xp5pB835ssbJdAVLcsVmNUc" +
       apiKey;
 
     const payload = {
